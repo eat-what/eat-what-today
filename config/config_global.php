683e8f4800e53fdc5e3209c7ce02171742884dc7
ssp <laokiea@163.com>
2018-10-16 14:41:43 +0800

    去除config

--- config/config_global.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    // "aes_key" => hash_hmac("sha256", "EatWhatAesKey", "ewak");
-    "pub_key_pem_file" => "file://".SOURCE_PATH."Public".DS."secret".DS."api_pub_key.pem",
-    "pri_key_pem_file" => "file://".SOURCE_PATH."Public".DS."secret".DS."api_private_key.pem",
-];
