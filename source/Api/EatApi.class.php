4509d67ffe874e931a298ea6b500f3810f46ed1d
laokiea <laokiea@163.com>
2018-10-17 10:47:15 +0800

    tst

--- source/Api/EatApi.class.php
+++ source/Api/EatApi.class.php
@@ -34,7 +34,7 @@ class EatApi extends ApiBase
         $verifyResult = $this->verifyGithubWebHookSignature();
         if( $verifyResult ) {
             $cmd = "cd /web/www/eat-what/ && git pull --rebase";
-            pclose(popen($cmd, "r"));
+            //pclose(popen($cmd, "r"));
             echo "Success";
         } else {
             EatWhatLog::logging("Illegality Github WebHook Request", [
